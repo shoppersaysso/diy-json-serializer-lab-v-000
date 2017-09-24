f698d9cb9b41f36bcb39c1702dcf19945d510ebc
Sara Hopper <sara.hopper@gmail.com>
2017-09-24 02:19:29 +0000

    automatically backed up by learn

--- app/serializers/product_serializer.rb
+++ app/serializers/product_serializer.rb
@@ -4,7 +4,7 @@ class ProductSerializer
     serialized_product = '{'
 
     serialized_product += '"id": ' + product.id.to_s + ', '
-    serialized_product += '"title": "' + product.name + '", '
+    serialized_product += '"name": "' + product.name + '", '
     serialized_product += '"price": "' + product.price + '", '
     serialized_product += '"inventory": "' + product.inventory + '", '
     serialized_product += '"description": "' + product.description + '"'
